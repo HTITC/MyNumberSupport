cdbd75d4e827c95b21ce41b317906de3c33a7fdc
JandosKh <jandos-halik@ht-tax.or.jp>
2015-10-24 22:10:53 +0900

    login form

--- server.js
+++ server.js
@@ -8,14 +8,37 @@ var handlebars = require('express-handlebars')
     .create({
         defaultLayout:'main',
         extname: 'hbs',
-        //layoutsDir="views/layouts/"
+        //layoutsDir="views/layouts/",
+        helpers: {
+            section: function(name, options){
+                if(!this._sections) this._sections = {};
+                this._sections[name] = options.fn(this);
+                return null;
+            }
+        }
     });
 app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs');
 
-
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('login');
+});
+
+app.get('/support', function (req, res) {
+    res.render('support');
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
 });
 
 var server = app.listen(3000, function () {
